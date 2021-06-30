6ed5f74c2f8610f8c5311bc01f7ea5a446e5ab9e
Tanmay Agrawal <tanmayagrawal2004@gmail.com>
2021-06-30 10:39:43 +0530

    Settings Modified to Signify this is the Dev Repo

--- tlp/settings.py
+++ tlp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_3^ltb1w73h_==wv&hq$6kxw!b*rx%sas208akcumbcs!0ra#q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
