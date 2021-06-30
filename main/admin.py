edd5393f411eb7c684d50bf4b33030a21a42cbfe
tanmaya1-code <84491314+tanmaya1-code@users.noreply.github.com>
2021-06-30 20:09:44 +0530

    Delete main directory

--- main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import AbsenceType, Log, Student, Grade, Board
-# Register your models here.
-
-admin.site.register(Grade)
-admin.site.register(Student)
-admin.site.register(Board)
-admin.site.register(Log)
-admin.site.register(AbsenceType)
